6266b6a82b61649a4f8fe637f28395fd580b921d
TheChun <toyota.jimmy202@gmail.com>
2025-06-23 21:14:39 +0800

    F#20250623 : 新增使用者功能

--- src/app/pages/add-user/add-user.ts
+++ src/app/pages/add-user/add-user.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// 20250623 mod by jimmy for 新增使用者功能
+import { Router } from '@angular/router'; //路徑 : 新增成功後返回users頁面
+import { FormsModule } from '@angular/forms'; //管理使用者輸入資料
+
 
 @Component({
   selector: 'app-add-user',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-user.html',
-  styleUrl: './add-user.css'
+  styleUrls: ['./add-user.css']
 })
-export class AddUserComponent {
 
+// 20250623 mod by jimmy for 新增使用者功能
+export class AddUserComponent {
+  name = '';
+  email = '';
+
+  constructor(private router: Router) {} // 這個 component 具備路由導向能力
+
+  addUser() {
+    const newUser = {
+      id: Date.now(), // 暫時用 timestamp 當作 ID
+      name: this.name, // this.name表示使用者在前端輸入的資料
+      email: this.email // this.email表示使用者在前端輸入的資料
+    };
+
+    const users = JSON.parse(localStorage.getItem('users') || '[]'); //如果'users'不存在就回傳[]。用JSON.parse()把JSON轉為js的陣列或物件
+    users.push(newUser); // 推入新資料到陣列
+    localStorage.setItem('users', JSON.stringify(users));  // 把陣列的資料放入localStorage的'users'。用JSON.stringify()把users轉為JSON
+
+    this.router.navigate(['/users']); // 導回使用者清單頁
+  }
 }
--- src/app/pages/users/users.ts
+++ src/app/pages/users/users.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+// 20250623 mod by jimmy for 新增使用者功能
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,15 +10,44 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./users.css']
 })
 
-export class UsersComponent {
-  users = [
-      { id: 1, name: '王小明', email: 'xiaoming@example.com' },
-      { id: 2, name: '陳美麗', email: 'meili@example.com' },
-      { id: 3, name: '張大衛', email: 'david@example.com' }
-    ];
+// 20250623 mod by jimmy for 新增使用者功能
+export class UsersComponent implements OnInit {
+  // 20250623 mod by jimmy for 新增使用者功能
+  // users = [
+  //     { id: 1, name: '王小明', email: 'xiaoming@example.com' },
+  //     { id: 2, name: '陳美麗', email: 'meili@example.com' },
+  //     { id: 3, name: '張大衛', email: 'david@example.com' }
+  //   ];
 
-    deleteUser(id: number) {
-      console.log('要刪除 ID：', id);
-      this.users = this.users.filter(user => user.id !== id); //透過將users陣列做filter方法，保留id不同的剔除id相同的，實現刪除
+  // 20250623 mod by jimmy for 新增使用者功能
+  users: { id: number, name: string, email: string }[] = [];
+  ngOnInit() {
+    const stored = localStorage.getItem('users'); // 從localStorage取出東西
+
+    // 先看localStorage有沒有東西
+    if (stored) {
+      this.users = JSON.parse(stored); // 有的話 : 把localStorage的users資料轉為js的資料型態
+    } else {
+      // 如果沒資料的話 : 用土炮的方式初始化預設資料
+      this.users = [
+        { id: 1, name: '王小明', email: 'xiaoming@example.com' },
+        { id: 2, name: '陳美麗', email: 'meili@example.com' },
+        { id: 3, name: '張大衛', email: 'david@example.com' }
+      ];
+      localStorage.setItem('users', JSON.stringify(this.users));
     }
+  }
+
+  deleteUser(id: number) {
+    const user = this.users.find(u => u.id === id);
+
+    if (user && confirm('是否確定刪除 ? ')){
+      this.users = this.users.filter(u => u.id !== id); //透過將users陣列做filter方法，保留id不同的剔除id相同的，實現刪除
+
+      // 20250623 mod by jimmy for 新增使用者功能
+      localStorage.setItem('users', JSON.stringify(this.users)); // 透過filter方法更新後的使用者資料重新存回 localStorage
+      console.log(`${user.name} 已被刪除`);
+    }
+  }
 }
+
